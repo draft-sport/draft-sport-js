9f3c057aed5ba38cc9f731427b72f8b6ba96aa4d
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-01-09 16:59:32 +1100

    add `CommunicationMode` class

---
+++ draft_sport/communication/mode.js
@@ -0,0 +1,46 @@
+/* Draft Sport JS - Communication Mode Class */
+
+
+class CommunicationMode {
+
+    constructor(
+        indexId,  // Integer
+        name  // String
+    ) {
+
+        this._indexid = indexId;
+        this._name = name;
+        
+        return;
+
+    }
+
+    get indexid() { return this._indexid; }
+    get name() { return this._name; }
+
+    static get PHONE_NUMBER() { return new CommunicationMode(
+        1,
+        'phone'
+    )}
+
+    static get EMAIL_ADDRESS() { return new CommunicationMode(
+        2,
+        'email'
+    )}
+
+    static get enumerations() { return [
+        CommunicationMode.PHONE_NUMBER,
+        CommunicationMode.EMAIL_ADDRESS
+    ]}
+
+    static withId(indexid) {
+        const Self = CommunicationMode;
+        for (let i = 0; i < Self.enumerations.length; i++) {
+            if (Self.enumerations[i].indexid == indexid) {
+                return Self.enumerations[i];
+            }
+        }
+        return null;
+    }
+
+}
