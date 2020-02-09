d749b72ffebd1bd1a38c0e7bdc0959eec367f688
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-02-09 15:12:16 +1100

    add "points per minute played" to `Points`

--- draft_sport/fantasy/points/points.js
+++ draft_sport/fantasy/points/points.js
@@ -7,12 +7,14 @@ class Points {
         averagePoints,  // Integer
         totalPoints,  // Integer
         pointsLastRound,  // Integer
+        pointsPerMinutePlayed,  // Optional[Integer]
         rounds  // Array<Rounds>
     ) {
 
         this._averagePoints = averagePoints;
         this._totalPoints = totalPoints;
         this._pointsLastRound = pointsLastRound;
+        this._pointsPerMinutePlayed = pointsPerMinutePlayed;
         this._rounds = rounds;
 
         return;
@@ -22,6 +24,7 @@ class Points {
     get averagePoints() { return this._averagePoints; }
     get totalPoints() { return this._totalPoints; }
     get pointsLastRound() { return this._pointsLastRound; }
+    get pointsPerMinutePlayed() { return this._pointsPerMinutePlayed; }
     get rounds() { return this._rounds; }
 
     static decode(data) {  // -> Points
@@ -29,6 +32,7 @@ class Points {
             data['average_points'],
             data['total_points'],
             data['points_last_round'],
+            data['points_per_minute_played'],
             RoundPoints.decodeMany(data['rounds'])
         );
     }
