e55f0f37f95a8e82ffcac9ed00c1df7c3cbf14c5
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-02-16 12:39:04 +1100

    add `League` class

---
+++ draft_sport/leagues/league.js
@@ -0,0 +1,154 @@
+/* Draft Sport JS - League Class */
+
+class League {
+
+    static get _PATH() { return '/league' }
+    static get _LIST_PATH() { return '/league/list' }
+
+    constructor(
+        publicId,
+        commissionerId,
+        name,
+        created,
+        teams
+    ) {
+
+        this._publicId = publicId;
+        this._commissionerId = commissionerId;
+        this._name = name;
+        this._created = created;
+        this._teams = teams;
+
+        return;
+
+    }
+
+    get publicId() { return this._publicId; }
+    get commissionerId() { return this._commissionerId; }
+    get name() { return this._name; }
+    get created() { return this._created; }
+    get teams() { return this._teams; }
+
+    static decode(data) {
+        return new League(
+            data['public_id'],
+            data['commissioner_id'],
+            data['name'],
+            data['created'],
+            data['teams'].map((t) => { return LeagueTeam.decode(t); })
+        );
+    }
+
+    static decodeMany(data) {
+        return data.map((d) => { League.decode(d); });
+    }
+
+    static create(
+        commissionerId,  // String
+        name,  // String
+        season,  // Season
+        callback,  // Function(Error? League?)
+        session = null  // Optional[Session]
+    ) {
+
+        const Self = League;
+        const payload = {
+            'name': name,
+            'commissioner_id': commissionerId,
+            'season': season.name
+        }
+
+        const _ = new ApiRequest(
+            Self._PATH,
+            'POST',
+            null,
+            payload,
+            (error, data) => {
+                if (error != null) { callback(error, null); return; }
+                try { callback(null, Self.decode(data)); return;} 
+                catch (error) { callback(error, null); return; }
+            },
+            session
+        );
+
+        return;
+    }
+
+    static retrieveMany(
+        managerId,  // Optional[String]
+        commissionerId,  // Optional[String]
+        season,  // Season
+        callback,  // Function(Error?, Array<League>?)
+        limit = 10,
+        offset = 0,
+        session = null
+    ) {
+
+        const Self = League;
+
+        const rawParameters = [
+            new UrlParameter('offset', offset),
+            new UrlParameter('limit', limit)
+        ]
+
+        if (managerId != null) { rawParameters.push(
+            new UrlParameter('manager', managerId)
+        ); }
+
+        if (commissionerId != null) { rawParameters.push(
+            new UrlParameter('commissioner', commissionerId)
+        ); }
+
+        if (season != null) { rawParameters.push(
+            new UrlParameter('season', season.name)
+        ); }
+
+        const parameters = new UrlParameters(rawParameters);
+
+        const _ = new ApiRequest(
+            Self._LIST_PATH,
+            'GET',
+            parameters,
+            null,
+            (error, data) => {
+                if (error != null) { callback(error, null); return; }
+                try { callback(null, League.decodeMany(data)); return; }
+                catch (error) { callback(error, null); return; }
+            },
+            session
+        );
+
+        return;
+
+    }
+
+    static retrieve(
+        publicId,  // String
+        callback,  // Function(Error? League?)
+        session = null
+    ) {
+
+        const Self = League;
+
+        const rawParameters = [
+            new UrlParameter('league_id', publicId),
+        ];
+
+        const _ = new ApiRequest(
+            Self._PATH,
+            'GET',
+            new UrlParameters(rawParameters),
+            null,
+            (error, data) => {
+                if (error != null) { callback(error, null); return; }
+                try { callback(null, League.decode(data)); return; }
+                catch (error) { callback(error, null); return; }
+            },
+            session
+        );
+
+        return;
+
+    }
+
+}
