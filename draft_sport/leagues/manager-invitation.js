93a1da27cc38cba73d829076b89f70e04f398009
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-02-19 10:53:19 +1100

    remove some printf

--- draft_sport/leagues/manager-invitation.js
+++ draft_sport/leagues/manager-invitation.js
@@ -32,14 +32,39 @@ class ManagerInvitation {
             'season': '2020' // MVP hardcode
         }
 
-        console.log(payload);
-        console.log(leagueId);
-
         const _ = new ApiRequest(
             Self._PATH,
             'POST',
             null,
             payload,
+            (error, data) => {
+                if (error != null) { callback(error, null); return; }
+                try { callback(null, Self.decode(data)); return; }
+                catch (error) { callback(error, null); return; }
+            },
+            session
+        );
+
+        return;
+    }
+
+    static retrieve(
+        token,  // String
+        callback,  // Function(Error?, ManagerInvitation?)
+        session = null
+    ) {
+
+        const Self = ManagerInvitation;
+        
+        const parameters = new UrlParameters([
+            new UrlParameter('token', token)
+        ])
+
+        const _ = new ApiRequest(
+            Self._PATH,
+            'GET',
+            parameters,
+            null,
             (error, data) => {
                 if (error != null) { callback(error, null); return; }
                 try { callback(null, Self.decode(data)); return; }
