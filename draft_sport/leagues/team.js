81fe74bff53daedd9c14fd2dff6ab6a400c684c8
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-02-22 15:11:41 +1100

    add `LeagueTeam.name` property

--- draft_sport/leagues/team.js
+++ draft_sport/leagues/team.js
@@ -6,16 +6,23 @@ class LeagueTeam {
     constructor(
         leagueId,  // String
         picks,  // Array<Pick>
-        managerId  // String
+        managerId,  // String
+        name  // String
     ) {
 
         this._league_id = leagueId;
         this._picks = picks;
         this._managerId = managerId;
+        this._name = name;
 
         return;
 
     }
+
+    get leagueId() { return this._league_id; }
+    get picks() { return this._picks; }
+    get managerId() { return this._managerId; }
+    get name() { return this._name; }
 
     static decodeMany(data) {
         return data.map((t) => { return LeagueTeam.decode(t); } );
@@ -25,7 +32,8 @@ class LeagueTeam {
         return new LeagueTeam(
             data['league_id'],
             LeaguePick.decodeMany(data['picks']),
-            data['manager_id']
+            data['manager_id'],
+            data['name']
         );
     }
 
