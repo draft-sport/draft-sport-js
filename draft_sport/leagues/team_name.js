2bcd773cd69854e3c9c7fd6e6b56858c23d0a21e
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-02-23 20:30:31 +1100

    add `TeamName` class

---
+++ draft_sport/leagues/team_name.js
@@ -0,0 +1,60 @@
+/* Draft Sport JS - Team Name Class */
+
+
+class TeamName {
+
+    static get _PATH() { return '/league/team/name'; }
+
+    constructor(
+        name,  // String
+        managerId,  // String
+        leagueId  // String
+    ) {
+
+        this._name = name;
+        this._managerId = managerId;
+        this._leagueId = leagueId;
+
+        return;
+    }
+
+    static decode(data) {
+        return new TeamName(
+            data['name'],
+            data['manager_id'],
+            data['league_id']
+        );
+    }
+
+    static create(
+        name,  // String
+        leagueId,  // String
+        managerId,  // String
+        callback,  // Function(Error? TeamName?)
+        session=null  // Optional[Session]
+    ) {
+
+        const Self = TeamName;
+        const payload = {
+            'name': name,
+            'league': leagueId,
+            'manager': managerId
+        }
+
+        const _ = ApiRequest(
+            Self._PATH,
+            'POST',
+            null,
+            payload,
+            (error, data) => {
+                if (error != null) { callback(error, null); return; }
+                try { callback(null, Self.decode(data)); return;} 
+                catch (error) { callback(error, null); return; }
+            },
+            session
+        );
+
+        return;
+    }
+
+}
