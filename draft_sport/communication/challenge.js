796465fa03dfd138e5f4414a6d28d30754415038
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-02-29 11:45:11 +1100

    add `Challenge` class

---
+++ draft_sport/communication/challenge.js
@@ -0,0 +1,53 @@
+/* Draft Sport JS  - Challenge Class */
+
+
+class Challenge {
+
+    static get _PATH() { return '/communication-method/challenge'; }
+
+    constructor(
+        expiration,  // String
+        completed  // Boolean
+    ) {
+
+        this._expiration = expiration;
+        this._completed = completed;
+
+        return;
+
+    }
+
+    static decode(data) {
+        return new Challenge(
+            data['expiration'],
+            data['completed']
+        );
+    }
+
+    static create(
+        methodId,  // String
+        callback,  // Function(Error?, Challenge?)
+        session  // Optional[Session]
+    ) {
+
+        const Self = Challenge;
+        const payload = { 'method': methodId };
+
+        const _ = new ApiRequest(
+            Self._PATH,
+            'POST',
+            null,
+            payload,
+            (error, data) => {
+                if (error != null) { callback(error, null); return; }
+                try { callback(null, Self.decode(data)); return;} 
+                catch (error) { callback(error, null); return; }
+            },
+            session
+        );
+
+        return;
+
+    }
+
+}
