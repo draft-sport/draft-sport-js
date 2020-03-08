109799c2828572b8e1469c5c56b6c16ccd6ef046
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-03-08 12:59:01 +1100

    add `Subscription` class

---
+++ draft_sport/store/subscription.js
@@ -0,0 +1,64 @@
+/* Draft Sport JS - Subscription Class */
+
+
+class Subscription {
+
+    static get _PATH() { return '/subscription'; }
+
+    constructor(
+        humanId,  // String
+        offerId,  // String
+        created  // String
+    ) {
+        this._humanId = humanId;
+        this._offerId = offerId;
+        this._created = created;
+
+        return;
+    }
+
+    static decode(data) {
+        return new Subscription(
+            data['human_id'],
+            data['offer_id'],
+            data['created']
+        );
+    }
+
+    static create(
+        token, // String (Stripe token)
+        humanId,  // String
+        offerId,  // String
+        iso_4217,  // String
+        contribution,  // Optional[Integer]
+        callback,  // Function(Error?, Subscription?)
+        session=null,  // Optional[Session]
+    ) {
+
+        const Self = Subscription;
+
+        payload = {
+            'token': token,
+            'human': humanId,
+            'offer': offerId,
+            'currency': iso_4217,
+            'development_contribution': contribution
+        }
+
+        const _ = new ApiRequest(
+            Self._PATH,
+            null,
+            payload,
+            (error, data) => {
+                if (error != null) { callback(error, null); return; }
+                try { callback(null, Self.decode(data)); return;} 
+                catch (error) { callback(error, null); return; } 
+            },
+            session
+        );
+
+        return;
+
+    }
+
+}
