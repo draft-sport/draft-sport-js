dbf0a123cf51f678224b97edbca078be755e0d89
Hugh Jeremy <hugh.jeremy@gmail.com>
2020-03-23 15:12:01 +1100

    add `Composition` class

---
+++ draft_sport/leagues/composition.js
@@ -0,0 +1,45 @@
+/* Draft Sport JS - Composition Class */
+
+
+class Composition {
+
+    static get _PATH() { return '/leagues/team/composition' }
+
+    constructor(
+        requirements
+    ) {
+        this._requirements = requirements;
+        return;
+    }
+
+    get requirements() { return this._requirements; }
+
+    static decode(data) {
+        return new Composition(
+            Requirement.decodeMany(data['requirements'])
+        );
+    }
+
+    static retrieve(
+        callback,  // Function(Error?, Composition?)
+        session=null  // Optional[Session]
+    ) {
+
+        const Self = Composition;
+
+        const _ = new ApiRequest(
+            Self._PATH,
+            'GET',
+            null,
+            null,
+            (error, data) => {
+                if (error) { callback(error, null); return; }
+                try { callback(null, Self.decode(data)); return; }
+                catch (error) { callback(error, null); return; }
+            }
+        );
+
+        return;
+    }
+
+}
